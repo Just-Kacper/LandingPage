0f8fecef16a1a8710e19f776e4fdbed1d02fd203
Kacper <kacperpaul@icloud.com>
2024-07-03 14:55:49 +0200

    initialize commit

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/global.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div>
+        <h1 className="text-3xl">Landing Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
